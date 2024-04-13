af6909a6212b7c6d09d360fbe855d6b79f56eec9
Ajay Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2024-04-13 13:07:59 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,5 @@
 <?php
-  $receiving_email_address = 'dhangarajay13@gmail.com';
+  $receiving_email_address = 'ajaydhangar49@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -16,14 +16,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.mailgun.org',
+    'username' => 'postmaster@codeharborhub.live',
+    'password' => '5e8c510cf534f41d86e2d189f6815f0c-4b670513-5242016a',
     'port' => '587'
   );
-  */
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
